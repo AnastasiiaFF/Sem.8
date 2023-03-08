a135f0da8bd19e33ff9e4f911ecb42492f271f10
Anastasiia Filatova <anastasia.m088@yandex.ru>
2023-03-08 12:03:58 +0300

    55-1

--- task53/Program.cs
+++ task53/Program.cs
@@ -38,14 +38,14 @@ void Replace(int[,] matrix)
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         int temp = matrix[0, j];
-        matrix[0, j] = matrix[lastRow, j];
+        matrix[firstRow, j] = matrix[lastRow, j];
         matrix[lastRow, j] = temp;
     }
 }
 
 
 
-int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
 PrintMatrix(array2d);
 Replace(array2d);
 Console.WriteLine();
